90650630cf6eb26c4c61c7d84d8e2ac058743490
Jose-pat <64907489+Jose-pat@users.noreply.github.com>
2020-05-09 12:42:00 -0500

    aqui esta lo del indexcontroller de la aplicacion

---
+++ indexController.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import com.example.demo.controller.models.Lista;
+
+
+@Controller
+public class indexController {
+
+	@GetMapping ({"/indexl", "/,"})
+	public String index(Model model) {
+		model.addAttribute("titulo", "Bienvenidos al MERCADO FTC´s");
+		
+		return "indexl";
+	}
+	
+	@ModelAttribute ("listas")
+	public List<Lista> poblarLista() {				
+		List<Lista> listas = Arrays.asList(
+				new Lista("Gansito", "Marinela", "$15", "10", "Pastelito sabor chocolate", "Marinela"));		
+		
+		return listas;
+	}
+	
+}
